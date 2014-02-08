8141a65d8a170c5a6b5c511f62de47fb8be0cd7b
Tim Bell <tim@galacticcode.com>
2014-02-08 13:01:55 +1000

    Working on build

--- build/myapp.rb
+++ build/myapp.rb
@@ -12,8 +12,16 @@ class MyApp
     @git
   end
 
+  def get_commit_id
+    get_git.object('HEAD^').blob?
+  end
+
+  def get_current_branch
+    get_git.branches.local
+  end
+
   def get_release_version
-    get_git.status
+    get_commit_id
   end
 
 end
