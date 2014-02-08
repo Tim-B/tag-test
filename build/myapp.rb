851f3cfa3c4d249e27e48f772192f5d25984c9bb
Tim Bell <tim@galacticcode.com>
2014-02-08 12:40:56 +1000

    Working on git infrustructure

--- build/myapp.rb
+++ build/myapp.rb
@@ -1,7 +1,19 @@
-module MyApp
+require 'git'
+require 'singleton'
+
+class MyApp
+
+  include Singleton
+
+  def get_git
+    if @git == nil
+      @git = Git.open('../')
+    end
+    @git
+  end
 
   def get_release_version
-
+    get_git.status
   end
 
 end
